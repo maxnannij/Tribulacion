3175fdede6c6da43f79740a37da71f5f0e52ae02
maxnannij <147406754+maxnannij@users.noreply.github.com>
2024-03-20 23:15:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   const guardarLocalButton = document.getElementById("guardar-local");
+  const agregarItemButton = document.getElementById("add-item");
 
   guardarLocalButton.addEventListener("click", function() {
     const hojaPersonaje = obtenerInformacionHojaPersonaje();
     localStorage.setItem("hojaPersonaje", JSON.stringify(hojaPersonaje));
     alert("La información se ha guardado localmente.");
+  });
+
+  agregarItemButton.addEventListener("click", function() {
+    agregarItemInventario();
   });
 
   function obtenerInformacionHojaPersonaje() {
@@ -13,5 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
       nombre: nombre,
       // Otros campos de la hoja de personaje...
     };
+  }
+
+  function agregarItemInventario() {
+    const inventarioList = document.getElementById("inventario-list");
+    const newItem = document.createElement("li");
+    newItem.innerHTML = `
+      <input type="text" class="item-input" placeholder="Nombre del Item">
+      <input type="number" class="item-counter" min="0" value="0">
+    `;
+    inventarioList.appendChild(newItem);
   }
 });
