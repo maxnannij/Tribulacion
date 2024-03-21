6895670d1698862cbeddc4d4623030e5445ec2fc
maxnannij <147406754+maxnannij@users.noreply.github.com>
2024-03-20 23:27:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const guardarLocalButton = document.getElementById("guardar-local");
+  const guardarCookieButton = document.getElementById("guardar-cookie");
   const agregarItemButton = document.getElementById("add-item");
 
-  guardarLocalButton.addEventListener("click", function() {
+  guardarCookieButton.addEventListener("click", function() {
     const hojaPersonaje = obtenerInformacionHojaPersonaje();
-    localStorage.setItem("hojaPersonaje", JSON.stringify(hojaPersonaje));
-    alert("La información se ha guardado localmente.");
+    guardarComoCookie("hojaPersonaje", JSON.stringify(hojaPersonaje));
+    alert("La información se ha guardado como cookie.");
   });
 
   agregarItemButton.addEventListener("click", function() {
@@ -20,22 +20,26 @@ document.addEventListener("DOMContentLoaded", function() {
     };
   }
 
+  function guardarComoCookie(nombreCookie, valorCookie, expiracionDias) {
+    let cookieString = nombreCookie + "=" + valorCookie;
+
+    if (expiracionDias) {
+      const fechaExpiracion = new Date();
+      fechaExpiracion.setTime(fechaExpiracion.getTime() + (expiracionDias * 24 * 60 * 60 * 1000));
+      cookieString += "; expires=" + fechaExpiracion.toUTCString();
+    }
+
+    document.cookie = cookieString;
+  }
+
   function agregarItemInventario() {
     const inventarioList = document.getElementById("inventario-list");
     const newItem = document.createElement("li");
     newItem.innerHTML = `
       <input type="text" class="item-input" placeholder="Nombre del Item">
       <input type="number" class="item-counter" min="0" value="0">
-      <button class="delete-item">Borrar</button>
     `;
     inventarioList.appendChild(newItem);
-
-    // Agregar evento de clic al botón de borrar
-    const deleteButton = newItem.querySelector(".delete-item");
-    deleteButton.addEventListener("click", function() {
-      if (confirm("¿Estás seguro de que deseas borrar este ítem?")) {
-        newItem.remove();
-      }
-    });
   }
 });
+
